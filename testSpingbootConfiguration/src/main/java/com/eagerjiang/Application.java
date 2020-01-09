4730cc945db376793302eaf9f6d1003ba9d62c0f
jiangqw <jiangqw@chinacscs.com>
2020-01-09 16:42:12 +0800

    feat: finish reading chapter3 and add a test module
    Signed-off-by: jiangqw <jiangqw@chinacscs.com>

---
+++ testSpingbootConfiguration/src/main/java/com/eagerjiang/Application.java
@@ -0,0 +1,17 @@
+package com.eagerjiang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author jiangqw
+ * @date 2020/1/7 17:55
+ */
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
