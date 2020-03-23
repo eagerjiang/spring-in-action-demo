5b70a7eaa21d8b7744cb4515de7e33d817710dbe
jiangqw <jiangqw@chinacscs.com>
2020-03-23 19:01:01 +0800

    feat: finished reading chapter7
    Signed-off-by: jiangqw <jiangqw@chinacscs.com>

--- chapter7/src/main/java/com/myapp/contoller/SpitterController.java
+++ chapter7/src/main/java/com/myapp/contoller/SpitterController.java
@@ -4,12 +4,14 @@ import com.myapp.Spitter;
 import com.myapp.data.SpitterRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
 import java.io.File;
@@ -39,5 +41,27 @@ public class SpitterController {
         profilePicture.transferTo(new File("/data/spittr/" + profilePicture.getOriginalFilename()));
         spitterRepository.save(spitter);
         return "redirect:/spitter/" + spitter.getUsername();
+    }
+
+    /**
+     * 模型数据最终是以请求参数的形式复制到请求中的
+     *
+     * @param spitter Spitter
+     * @param model   Model
+     * @return view
+     */
+    @PostMapping(value = "/register1")
+    public String processRegistration(Spitter spitter, Model model) {
+        spitterRepository.save(spitter);
+        model.addAttribute("username", spitter.getUsername());
+        return "redirect:/spitter/{username}";
+    }
+
+    @PostMapping(value = "/register2")
+    public String processRegistration(Spitter spitter, RedirectAttributes model) {
+        spitterRepository.save(spitter);
+        model.addAttribute("username", spitter.getUsername());
+        model.addFlashAttribute("spitter", spitter);
+        return "redirect:/spitter/{username}";
     }
 }
